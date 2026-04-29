a545a5ec09d409da1c56150bae0cf39d116cfeaf
tomahelsa16 <tomahelsa16@gmail.com>
2026-04-29 12:59:44 +0700

    add some folders and files
    add theme.dart, api_services.dart, routes.dart, Product.dart, Cart.dart, main.dart, keyboard.dart, enums.dart, constants.dart

---
+++ lib/models/Cart.dart
@@ -0,0 +1,24 @@
+import 'Product.dart';
+
+class Cart {
+  final String name;
+  final int price;
+  final int numOfItem;
+  final int totalPrice;
+
+  Cart({
+    required this.name,
+    required this.price,
+    required this.numOfItem,
+    required this.totalPrice,
+  });
+
+  factory Cart.fromJson(Map<String, dynamic> json) {
+    return Cart(
+      name: json['name'],
+      price: json['price'],
+      numOfItem: json['count'],
+      totalPrice: json['total_price'],
+    );
+  }
+}
